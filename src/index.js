e398635d81ace6ca1d416ee58bc571de437ff397
ArturVargas <arturvargas9226@gmail.com>
2023-07-22 22:00:58 +0200

    add filtered route

--- src/index.js
+++ src/index.js
@@ -1,37 +1,39 @@
-import 'dotenv/config';
-import cors from 'cors';
-import routes from './routes';
+import "dotenv/config";
+import cors from "cors";
+import routes from "./routes";
 
-const express = require('express')
+const express = require("express");
 
-import models, { sequelize } from './models';
+import models, { sequelize } from "./models";
 
-const bodyParser = require('body-parser')
-const app = express()
-const port = process.env.HTTP_PORT
+const bodyParser = require("body-parser");
+const app = express();
+const port = process.env.HTTP_PORT;
 
 app.use(cors());
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
-)
+);
 
-app.use('/api', routes.api);
-app.use('/dashboard', routes.dashboard);
-app.use('/healthcheck', routes.healthcheck);
-app.use('/', routes.provisioning);
-app.use('/rpc', routes.rpc);
-
+app.use("/api", routes.api);
+app.use("/dashboard", routes.dashboard);
+app.use("/healthcheck", routes.healthcheck);
+app.use("/", routes.provisioning);
+app.use("/rpc", routes.rpc);
+app.use("/filtered", routes.filtered);
 
 // A home page for your add-on
-app.get('/', (request, response) => {
-  response.send('A QuickNode Marketplace Add-On example build with node.js, Express, and PostgreSQL')
-})
+app.get("/", (request, response) => {
+  response.send(
+    "A QuickNode Marketplace Add-On example build with node.js, Express, and PostgreSQL"
+  );
+});
 
 sequelize.sync().then(() => {
   app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
-  })
+    console.log(`App running on port ${port}.`);
+  });
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,15 @@
-import api from './api';
-import dashboard from './dashboard';
-import healthcheck from './healthcheck';
-import provisioning from './provisioning';
-import rpc from './rpc';
+import api from "./api";
+import dashboard from "./dashboard";
+import healthcheck from "./healthcheck";
+import provisioning from "./provisioning";
+import rpc from "./rpc";
+import filtered from "./filtered";
 
 export default {
   api,
   dashboard,
   healthcheck,
   provisioning,
-  rpc
+  rpc,
+  filtered,
 };
